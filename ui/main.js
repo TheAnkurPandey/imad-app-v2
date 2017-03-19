3cc58643f64855dabd967ef1aa76d1c5faad99af
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-19 16:13:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
-    document.getElementById('madi').style.marginLeft = marginLeft + 'px';
+    img.style.marginLeft = marginLeft + 'px';
 }
 function myFunction() {
     var interval = setInterval(moveRight , 100);
