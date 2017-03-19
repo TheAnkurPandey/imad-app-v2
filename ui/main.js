c5b81e7b5b841fe242e9b7508f3b0df1c85dd16d
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-19 19:25:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,12 +52,32 @@ var submit = document.getElementById('submit_btn');
 function  s1() {
     //Make a request to the server and send the name
     
-    //Capture a list of names and render it as a list
-    var names = ['name1' , 'name2' , 'name3', 'name4'];
-    var list = '';
-    for(var i = 0 ; i<names.length ; i++){
-        list += '<li>' + names[i] + '</li>' ;
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //Create a request object
+   var request = new XMLHttpRequest();
+   
+   //Capture the response and store it in a variable
+   request.onreadystatechange = function (){
+       if (request.readyState === XMLHttpRequest.DONE) {
+            // everything is good, the response is received
+            if (request.status === 200) {
+            //Capture a list of names and render it as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i = 0 ; i<names.length ; i++){
+                list += '<li>' + names[i] + '</li>' ;
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+                
+            }
+       } 
+       //not done yet
+   };
+   
+   //Make the request 
+   request.open('GET', 'http://theankurpandey.imad.hasura-app.io/submit-name?name=' + name , true);
+   request.send(null);
+    
+    
 }
