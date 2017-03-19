8913559038f727994eecf41e9546ea8e3a4dfcb8
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-19 15:46:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML = "HI Ankur Pandey";
 
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onClick = function () {
+    var interval = setInterval(moveRight , 100);
     img.style.marginLeft = '100px';
 };
