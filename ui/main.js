e0b0191a683947f92b7a34ba0c24f5d299abf239
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-19 16:30:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,5 @@
 console.log('Loaded!');
 
-//change the text of main-text div
-var element = document.getElementById("main-text");
-element.innerHTML = "HI Ankur Pandey";
-
 //move the image
 var img = document.getElementById('madi');
 var marginLeft = 0;
