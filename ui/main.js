93886968f66d2509a037338d180b4b2cf368c5ee
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-19 16:53:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,4 +10,19 @@ function moveRight () {
 function myFunction() {
     var interval = setInterval(moveRight , 100);
 }
-console.log('Hi ankur pandey');
+
+//counter code
+var button = getElementById('counter');
+var counter = 0;
+button.onClick =function() {
+   //Make a request to counter endpoint
+   
+   //Capture the response and store it in a variable
+   
+   //Render the variable in correct span
+   counter = counter + 1;
+   var span = document .getElementById('count'); 
+   span.innerHTML = counter.toString();
+   
+};
+
