6aaf9bbc743dbd9f0978adce1e729fbc2a238786
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-21 21:04:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool =require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
   user: 'theankurpandey',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var articles = {
          'article-one' : {
@@ -106,6 +108,49 @@ function hash(input , salt){
 app.get('/hash/:input', function (req, res) {
   var hashedString = hash(req.params.input, 'this-is-some-random-string');
   res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+  //username, password
+  //json
+  var username = req.body.username;
+  var password = req.body.password;
+  var salt = crypto.randomBytes(128).toString('hex');
+  var dbString = hash(password, salt);
+  pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)',[username, dbString],function(err, result){
+   if (err){ 
+        res.status(500).send(err.toString());
+    }else{
+        res.send('User sucessfully created: '+ username);
+    }   
+  });
+});
+
+app.post('/login', function(req, res){
+   var username = req.body.username;
+  var password = req.body.password;
+  
+  pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err, result){
+   if (err){ 
+        res.status(500).send(err.toString());
+    }else{
+        if(result.rows.length === 0){
+            res.send(403).send('username/password is invalid');
+        }else{
+            //match the password
+            var dbString = result.rows[0].password;
+            var salt = dbString.split('$')[2];
+            var hashedPassword = hash(password, salt);
+            if(hashedPassword === dbString){
+               res.send('credentials correct!'); 
+            }else{
+                res.send(403).send('username/password is invalid');
+            }
+            
+        }
+        
+    }   
+  }); 
 });
 
 var pool = new Pool(config);
