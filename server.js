1a7a10e3a62ec319adb24c8cfa01a54f70406300
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-21 10:29:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool =require('pg').Pool;
+var config = {
+  user: 'theankurpandey',
+  database: 'theankurpandey',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -85,6 +93,21 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  //make a select request
+  //return a response with result
+   pool.query('INSERT INTO visit (date) VALUES ($1)', [new Date()], function(err) {
+    if (err){ 
+        res.status(500).send(err.toString());
+    }else{
+        res.send(JSON.stringify(result));
+    }
+    
+   });
+  
 });
 
 var counter = 0;
