d3fd7f73f813676e3f24697b8c72493840fe652d
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-21 22:37:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,94 +1,138 @@
-console.log('Loaded!');
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
+function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register to unlock awesome features</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
+    
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
 }
-function myFunction() {
-    var interval = setInterval(moveRight , 100);
+
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
 }
 
-//counter code
-/*
-var button = document.getElementById('counter');
-//var counter = 0;
-
-function b1() {
-   //Create a request object
-   var request = new XMLHttpRequest();
-   
-   //Capture the response and store it in a variable
-   request.onreadystatechange = function (){
-       if (request.readyState === XMLHttpRequest.DONE) {
-            // everything is good, the response is received
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-            // perfect!
-                var counter = request.responseText;
-                var span = document.getElementById('count'); 
-                span.innerHTML = counter.toString();
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
-       } 
-       //not done yet
-   };
-*/   
-   //Render the variable in correct span
-   /*counter = counter + 1;
-   var span = document.getElementById('count'); 
-   span.innerHTML = counter.toString();*/
-/*   
-   //Make the request 
-   request.open('GET', 'http://theankurpandey.imad.hasura-app.io/counter' , true);
-   request.send(null);
-   
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
 }
-*/
 
-//Submt username/password to login
-var submit = document.getElementById('submit_btn');
-function  s1() {
-    //Make a request to the server and send the name
-    
-    //Create a request object
-   var request = new XMLHttpRequest();
-   
-   //Capture the response and store it in a variable
-   request.onreadystatechange = function (){
-       if (request.readyState === XMLHttpRequest.DONE) {
-            // everything is good, the response is received
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
             if (request.status === 200) {
-            //Capture a list of names and render it as a list
-            /*var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-            for(var i = 0 ; i<names.length ; i++){
-                list += '<li>' + names[i] + '</li>' ;
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
             }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
-            */
-            console.log('user logged in');
-            alert('Logged in sucessfully');
-            
-            }else if(request.status === 403){
-                alert('Username/password is incorrect');
-            }else if(request.status === 500){
-                alert('Something went wrong on server');
-            }
-       } 
-       //not done yet
-   };
-   
-   //Make the request 
-   var username = document.getElementById('username').value;
-   var password = document.getElementById('password').value;
-   console.log(username);
-   console.log(password);
-   request.open('POST', 'http://theankurpandey.imad.hasura-app.io/login' , true);
-   request.setRequestHeader('Content-Type' , 'application/json');
-   request.send(JSON.stringify({username : username , password : password}));
+        }
+    };
     
-    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
 }
+
+
+// The first thing to do is to check if the user is logged in!
+loadLogin();
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
