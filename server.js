d162bc9145bd9c1c33ee8703c080d8d09bdef17e
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-03-23 22:02:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,6 +133,7 @@ app.get('/check-login', function (req, res) {
        res.status(400).send('You are not logged in');
    }
 });
+
 app.get('/logout', function (req, res) {
    delete req.session.auth;
    res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
