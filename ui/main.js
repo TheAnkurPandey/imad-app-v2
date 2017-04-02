08c9597111b649e01ca942b7ad6d9022760ca7c7
TheAnkurPandey <ankur.pandey1996@gmail.com>
2017-04-02 20:20:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,7 @@ function loadLoginForm () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
+                  alert('You are logged in successfully');
                   submit.value = 'Sucess!';
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
